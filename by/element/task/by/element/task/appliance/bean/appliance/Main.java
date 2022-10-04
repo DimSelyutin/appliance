68db7773e9b4053ed4c092d4af581ba5ba04af94
Selyutin Dmitriy <dima.selyutn.9@mail.ru>
2022-10-04 15:14:17 +0300

    del

--- by/element/task/by/element/task/appliance/bean/appliance/Main.java
+++ by/element/task/by/element/task/appliance/bean/appliance/Main.java
@@ -1,33 +1,29 @@
 package by.element.task.appliance.bean.appliance;
 
+
 import java.io.IOException;
 import java.io.Serializable;
 
-import by.element.task.appliance.bean.appliance.database.FileReader;
-import by.element.task.appliance.bean.appliance.database.GenerateBaseFile;
+import by.element.task.appliance.bean.appliance.helper.GenerateBaseFile;
 import by.element.task.appliance.bean.appliance.logic.FindApliance;
+import by.element.task.appliance.bean.appliance.view.ConsoleOutPut;
 
 public class Main implements Serializable {
     private static final long serialVersionUID = -4862926644813433707L;
 
     public static void main(String[] args) {
         System.out.println("To search apliance choose parametr that you need");
+        
         FindApliance find = new FindApliance();
-        // find.findByCore();
-    
-        FileReader fr = new FileReader();
-        try {
-            fr.readFile();
-        } catch (ClassNotFoundException | IOException e) {
-            e.printStackTrace();
-        }
-        // try {
-        //     Object obj = fr.readFile();
-        //     System.out.println(obj.toString());
-        // } catch (ClassNotFoundException | IOException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
-        // }
+        ConsoleOutPut console = new ConsoleOutPut();
+
+
+        console.print(find.byCore(6));
+        
+        
+       
+   
+       
 
 
     }
