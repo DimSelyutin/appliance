5cac659c286018d3ff4ae60a912aaa1a7f9a77e5
Selyutin Dmitriy <dima.selyutn.9@mail.ru>
2022-10-13 12:02:59 +0300

    update

--- by/element/task/Main.java
+++ by/element/task/Main.java
@@ -38,66 +38,67 @@ public class Main {
 
     private static Property propCollector(){
 
-        Scanner sc = new Scanner(System.in);
-        Map<String, Object> prop = new HashMap<>();
+        try (Scanner sc = new Scanner(System.in)) {
+            Map<String, Object> prop = new HashMap<>();
 
 
-        System.out.println("To search apliance enter parametrs that you need:");
-        System.out.print("Enter appliance group: ");
-        String appGroup = sc.nextLine();
+            System.out.println("To search apliance enter parametrs that you need:");
+            System.out.print("Enter appliance group: ");
+            String appGroup = sc.nextLine();
 
 
-        System.out.println("To skip the parameter press Enter!");
-        System.out.println("Enter parametrs of "+appGroup+":");
+            System.out.println("To skip the parameter press Enter!");
+            System.out.println("Enter parametrs of "+appGroup+":");
 
 
-        switch (appGroup) {
-            case "Oven":
-                for (Oven string : Oven.values()) {
-                    System.out.print(string+": ");
-                    prop.put(string.toString(), sc.nextLine());
-                }
-                break;
+            switch (appGroup) {
+                case "Oven":
+                    for (Oven string : Oven.values()) {
+                        System.out.print(string+": ");
+                        prop.put(string.toString(), sc.nextLine());
+                    }
+                    break;
 
-            case "Laptop":
-                for (Laptop string : Laptop.values()) {
-                    System.out.print(string+": ");
-                    prop.put(string+"", sc.nextLine());
-                }
-                break;
+                case "Laptop":
+                    for (Laptop string : Laptop.values()) {
+                        System.out.print(string+": ");
+                        prop.put(string+"", sc.nextLine());
+                    }
+                    break;
 
-            case "Refrigerator":
-                for (Refrigerator string : Refrigerator.values()) {
-                    System.out.print(string+": ");
-                    prop.put(string+"", sc.nextLine());
-                }
-                break;
+                case "Refrigerator":
+                    for (Refrigerator string : Refrigerator.values()) {
+                        System.out.print(string+": ");
+                        prop.put(string+"", sc.nextLine());
+                    }
+                    break;
 
-            case "VacuumCleaner":
-                for (VacuumCleaner string : VacuumCleaner.values()) {
-                    System.out.print(string+": ");
-                    prop.put(string+"", sc.nextLine());
-                }
-                break;
+                case "VacuumCleaner":
+                    for (VacuumCleaner string : VacuumCleaner.values()) {
+                        System.out.print(string+": ");
+                        prop.put(string+"", sc.nextLine());
+                    }
+                    break;
 
-            case "TabletPC":
-                for (TabletPC string : TabletPC.values()) {
-                    System.out.print(string+": ");
-                    prop.put(string+"", sc.nextLine());
-                }
-                break;
-            
-            case "Speakers":
-                for (Speakers string : Speakers.values()) {
-                    System.out.print(string+": ");
-                    prop.put(string+"", sc.nextLine());
-                }
-                break;
-            default:
-                System.out.println("Check the entered parameters or skip them.");
-                break;
+                case "TabletPC":
+                    for (TabletPC string : TabletPC.values()) {
+                        System.out.print(string+": ");
+                        prop.put(string+"", sc.nextLine());
+                    }
+                    break;
+                
+                case "Speakers":
+                    for (Speakers string : Speakers.values()) {
+                        System.out.print(string+": ");
+                        prop.put(string+"", sc.nextLine());
+                    }
+                    break;
+                default:
+                    System.out.println("Check the entered parameters or skip them.");
+                    break;
+            }
+            return new Property(appGroup, prop);
         }
-        return new Property(appGroup, prop);
        
     } 
 
